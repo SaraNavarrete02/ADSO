40ed0b71801c006471af3bc5e0f449e7639c0603
Daniel Sulaiman <125513682+DanSou1@users.noreply.github.com>
2026-06-20 21:02:17 -0500

    terminación de actividades Daniel

--- Actividad1/vectores.js
+++ Actividad1/vectores.js
@@ -1,1 +1,10 @@
-console.log("Este programa leerá 2 vectores enteros ordenados y generará una lista ordenada de los 2")
+let vector1 = [1, 3, 6, 9, 17];
+let vector2 = [2, 4, 10, 17, 20];
+
+console.log("------------------------------------------------------------")
+console.log("Este programa leerá 2 vectores y generará una lista ordenada")
+console.log("------------------------------------------------------------")
+
+const vectorOrdenado = vector1.concat(vector2).sort((a, b) => a - b);
+
+console.log("El vector ordenado es: " + vectorOrdenado);
