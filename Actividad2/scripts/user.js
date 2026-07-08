daa99adab42b9fb2ee572ac2241f98409135381e
Daniel Sulaiman <125513682+DanSou1@users.noreply.github.com>
2026-07-08 00:02:02 -0500

    Terminación de register
    Se completa el registro de usuario y su notificación.

--- Actividad2/scripts/user.js
+++ Actividad2/scripts/user.js
@@ -8,15 +8,22 @@ class User {
             email: "daniel@gmail.com",
             password: "1234"
         };
-        if (this.email === email && this.password === password) {
-            alert("Login correcto");
+        if (usuario.email == email && usuario.password === password) {
+            alert("ingreso correcto");
         } else {
             alert("Correo o contraseña incorrectos");
         }
     }
-    register() {
-
-        
+    register(names, lastNames, city, dateOfBirth, email, password) {
+        const nUser = {
+            names: names,
+            lastNames: lastNames,
+            city: city,
+            dateOfBirth: dateOfBirth,
+            email: email,
+            password: password
+        }
+        alert('Hola ' + nUser.names + ' ' + nUser.lastNames + ', tu registro fue exitoso, tu correo es: ' + nUser.email + ' tu ciudad es: ' + nUser.city + ' y tu fecha de nacimiento es: ' + nUser.dateOfBirth);
     }
 }
 /* 
@@ -25,11 +32,30 @@ class User {
     Por ultimo capturo los datos, creo un nuevo objeto y le doy los datos de correo y contraseña
     invocando el metodo login para su respectiva validacion.
 */
-document.getElementById('loginForm').addEventListener('submit', function (event) {
-    event.preventDefault();
-    const correoI = document.getElementById("email").value;
-    const psdI = document.getElementById("psd").value;
-    const nuevoUsuario = new User(correoI, psdI);
-    nuevoUsuario.login(correoI, psdI);
-});
+//Login
+const loginForm = document.getElementById('loginForm');
+if (loginForm) {
+    loginForm.addEventListener('submit', function (event) {
+        event.preventDefault();
+        const correoI = document.getElementById("email").value;
+        const psdI = document.getElementById("psd").value;
+        const nuevoUsuario = new User(correoI, psdI);
+        nuevoUsuario.login(correoI, psdI);
+    });
+}
 
+//Register
+const registerForm = document.getElementById('registerForm');
+if (registerForm) {
+    registerForm.addEventListener("submit", function (event) {
+        event.preventDefault();
+        const names = document.getElementById("names").value;
+        const lastNames = document.getElementById("lastNames").value;
+        const city = document.getElementById("city").value;
+        const dateOfBirth = document.getElementById("dateOfBirth").value;
+        const email = document.getElementById("email").value;
+        const password = document.getElementById("psd").value;
+        const nuevoUsuario = new User(email, password);
+        nuevoUsuario.register(names, lastNames, city, dateOfBirth, email, password);
+    });
+}
